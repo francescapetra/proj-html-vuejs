d9e5e2e2c21fb38d29e03e10c19ae11c0b196c7a
francescapetra <francescalaurapetra@gmail.com>
2021-04-19 12:49:09 +0200

    ciclo icone nav

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-// alert("ciao");
+var app = new Vue(
+  {
+    el: "#root",
+    data: {
+      iconsSocial:[
+        {
+          name: 'facebook-f',
+          family: 'fab ',
+          prefix: 'fa-',
+        },
+        {
+          name: 'twitter',
+          family: 'fab ',
+          prefix: 'fa-',
+        },
+        {
+          name: 'instagram',
+          family: 'fab ',
+          prefix: 'fa-',
+        },
+        {
+          name: 'youtube',
+          family: 'fab ',
+          prefix: 'fa-',
+        },
+    ],
+    },
+  }
+);
